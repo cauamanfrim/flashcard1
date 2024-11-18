eb5d0638696393aec5602c36c85d7362de04a12c
cauamanfrim <142427454+cauamanfrim@users.noreply.github.com>
2024-11-18 11:45:35 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,51 @@
+criaCartao(
+    'Futebol',
+    'Qantos mundiais o flamengo tem?',
+    'O flamengo tem um mundial'
+)
+
+
+criaCartao(
+    'futebol',
+    'Quantas copas do mundo tem o Brasil?',
+    'O Brasil tem cinco copas do mundo'
+)
+
+
+criaCartao(
+    'futebol',
+    'qual o maior time brasileiro?',
+    'O maior time do Brasil é o Santos'
+)
+
+
+criaCartao(
+    'Futebol',
+    'Qual o maior idolo da seleção brasileira?',
+    'Pelé'
+)
+
+
+criaCartao(
+    'Futebol',
+    'maior artilheiro do flamengo',
+    'Zico'
+)
+
+criaCartao(
+    'Futebol',
+    'qual o melhor time do brasil atualmente',
+    'botafogo'
+)
+
+criaCartao(
+    'Futebol',
+    'qual o maior time da Europa',
+    'Real madrid'
+)
+
+criaCartao(
+    'Futebol',
+    'melhor atacante da temporada',
+    'Vinicíus Junior'
+)
